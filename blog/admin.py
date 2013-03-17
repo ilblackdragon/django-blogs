74393211d1b4dc8710fe5521def67bae7aed25f7
frol <frolvlad@gmail.com>
2013-03-17 13:29:14 +0200

    add raw_id_fields for heavy foreignkeys

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,8 @@ from blog.models import Post, Blog, BlogUserAccess
 
 class BlogUserAccessInline(admin.TabularInline):
     model = BlogUserAccess
+    raw_id_fields = ('user', )
+
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
@@ -18,6 +20,8 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publish', 'status')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'body', 'tease')
+    raw_id_fields = ('author', 'rating')
+
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Post, PostAdmin)
