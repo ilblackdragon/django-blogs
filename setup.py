86b0fa1a99a243c28eb1b68686bb3d6bbf8eff68
Illia Polosukhin <ilblackdragon@gmail.com>
2013-08-05 03:06:40 +0000

    Updated setup to install locale folder when app is installed

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ MODULE_NAME = 'django-blogs'
 PACKAGE_NAME = 'blog'
 PACKAGE_DATA = list()
 
-for directory in [ 'templates', 'static' ]:
+for directory in [ 'templates', 'static', 'locale' ]:
     for root, dirs, files in os.walk( os.path.join( PACKAGE_NAME, directory )):
         for filename in files:
             PACKAGE_DATA.append("%s/%s" % ( root[len(PACKAGE_NAME)+1:], filename ))
